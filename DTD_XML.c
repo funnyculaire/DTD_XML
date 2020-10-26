90defcde69d8b1f6f7fabfff4f503b36c96f8a5e
Cheng Kevin <chengk.pro@gmail.com>
2020-10-26 20:06:47 +0100

    add DTD_XML.c

---
+++ DTD_XML.c
@@ -0,0 +1,8 @@
+//
+//  DTD_XML.c
+//  
+//
+//  Created by Kevin Cheng on 26/10/2020.
+//
+
+#include "DTD_XML.h"
---
+++ DTD_XML.h
@@ -0,0 +1,13 @@
+//
+//  DTD_XML.h
+//  
+//
+//  Created by Kevin Cheng on 26/10/2020.
+//
+
+#ifndef DTD_XML_h
+#define DTD_XML_h
+
+#include <stdio.h>
+
+#endif /* DTD_XML_h */
